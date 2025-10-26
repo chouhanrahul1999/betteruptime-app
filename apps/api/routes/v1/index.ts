80b2ea2f3a9b01686d43247f803f82b3521968d8
RahulSingh <chouhanrahul1999@gmail.com>
2025-10-26 19:54:12 +0530

    wrote basic api for website

--- apps/api/routes/v1/index.ts
+++ apps/api/routes/v1/index.ts
@@ -1,11 +1,12 @@
-import Router from "express"
-
+import { Router } from "express";
 import websiteRouter from "./website";
-
 
 const router = Router();
 
+router.get("/", (req, res) => {
+  res.json({ message: "API v1" });
+});
 
-router.use("/website", websiteRouter)
+router.use("/website", websiteRouter);
 
 export default router;
--- apps/api/routes/v1/website.ts
+++ apps/api/routes/v1/website.ts
@@ -1,13 +1,26 @@
 import { Router } from "express";
+import { prismaClient } from "db/client";
 
 const router = Router();
 
-router.post("/website", (req, res) => {
+router.post("/", async (req, res) => {
+  if (!req.body.url) {
+    res.status(400).json({
+      error: "Missing url",
+    });
+    return;
+  }
+  const website = await prismaClient.website.create({
+    data: {
+      url: req.body.url,
+      timeAdded: new Date(),
+    },
+  });
+  res.json({
+    id: website.id,
+  });
+});
 
-})
-
-router.get("/status/:websiteId", (req, res) => {
-
-})
+router.get("/status/:websiteId", (req, res) => {});
 
 export default router;
